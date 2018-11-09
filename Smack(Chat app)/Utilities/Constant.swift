6e2f7ee5ba51fe765b28ea43e77dd67ee1306396
PCR <PCR@IPCR-MBP.lan>
2018-11-10 04:24:51 +0700

    Lesson 9

--- Smack(Chat app)/Utilities/Constant.swift
+++ Smack(Chat app)/Utilities/Constant.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// url Constants
+let BASE_URL = "https://smackchat001.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+//segue
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannelVC"
+
+// User Default
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
