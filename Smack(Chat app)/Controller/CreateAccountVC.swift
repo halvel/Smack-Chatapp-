30b5902a248efa3607fb587a481c8b1d8c67a9df
PCR <PCR@IPCR-MBP.lan>
2018-11-10 05:08:54 +0700

    Lesson 10

--- Smack(Chat app)/Controller/CreateAccountVC.swift
+++ Smack(Chat app)/Controller/CreateAccountVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
     
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,24 @@ class CreateAccountVC: UIViewController {
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , email != "" else {
+            return
+        }
+        guard let password = passwordTxt.text , password != "" else {
+            return
+        }
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("user Register!")
+            }
+        }
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBgColorPressed(_ sender: Any) {
     }
     
 }
